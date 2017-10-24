a01bf2369c9240f5c898d01a7c38e5059a1bc087
Steven Eksteen <eksoverzero@outlook.com>
2017-10-24 20:00:42 +0200

    Console logging and alerts

--- js/main.js
+++ js/main.js
@@ -11,18 +11,18 @@ var user = {
 	}]
 };
 
-user.firstName //'Steven'
-user.lastName  //'Eksteen'
+console.log(user.firstName); //'Steven'
+console.log(user.lastName);  //'Eksteen'
 
-user.children         //[{name: 'Mark', age: 21}, {name: 'Mary', age: 45}]
-user.children[0]      //{name: 'Mark', age: 21}
-user.children[1].name //'Mary'
+console.log(user.children);         //[{name: 'Mark', age: 21}, {name: 'Mary', age: 45}]
+console.log(user.children[0]);      //{name: 'Mark', age: 21}
+console.log(user.children[1].name); //'Mary'
 
-user.firstName + user.lastName //'StevenEksteen'
+console.log(user.firstName + user.lastName); //'StevenEksteen'
 
-user.firstName + ' ' + user.lastName //'Steven Eksteen'
-`${user.firstName} ${user.lastName}` //'Steven Eksteen'
+console.log(user.firstName + ' ' + user.lastName); //'Steven Eksteen'
+console.log(`${user.firstName} ${user.lastName}`); //'Steven Eksteen'
 
-1 + 1   //2
-'1 + 1' //'1 + 1'
-`${0 + 1} + ${101 - 100}` //'1 + 1'
+console.log(1 + 1);   //2
+console.log('1 + 1'); //'1 + 1'
+console.log(`${0 + 1} + ${101 - 100}`); //'1 + 1'
