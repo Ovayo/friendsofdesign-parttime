c3eddf5deab335eec4a9e11761dc198b5679108e
Steven Eksteen <eksoverzero@outlook.com>
2017-11-08 18:29:07 +0200

    Menu scrolling

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ var header = document.getElementById('header');
 var content = document.getElementById('content');
 var mainMenu = document.getElementById('main-menu');
 
-function styleMainMenu(event) {
-	// If we are not at the top of the screen:
-	//   ...apply the scroll style to nav
-	//   ...else remove the scroll style from nav
-	// If we are more that one full screen window from the top:
-	//   ...apply the shadow stye to nav
-	//   ...else remove it
-	console.log(window.pageYOffset)
-}
-
 window.addEventListener("scroll", function(event){
 	var position = window.pageYOffset;
 
 	var nav = header.getElementsByTagName('nav')[0]
-	var classes = nav.classList.value
-	var hasClass = classes.indexOf('scroll') === -1
+	var hasScrollClass = nav.classList.value.indexOf('scroll') === -1
+	var hasShadowClass = nav.classList.value.indexOf('shadow') === -1
+
+	var totalHeaderHeight = header.offsetHeight - nav.offsetHeight
 
 	if(position > 1) {
-		if(hasClass){
-			nav.classList.value = classes + ' scroll';
+		if(hasScrollClass){
+			nav.classList.value = nav.classList.value + ' scroll';
 		}
 	} else {
-		nav.classList.value = classes.replace('scroll', '');
+		nav.classList.value = nav.classList.value.replace('scroll', '').trim();
+	}
+
+	if(position > totalHeaderHeight) {
+		if(hasShadowClass){
+			nav.classList.value = nav.classList.value + ' shadow';
+		}
+	} else {
+		nav.classList.value = nav.classList.value.replace('shadow', '').trim();
 	}
 });
