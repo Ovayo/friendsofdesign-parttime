5edbbffd70ea1c146a08be21efb24c1ebd246822
Steven Eksteen <eksoverzero@outlook.com>
2017-11-13 18:59:50 +0200

    Silly logic fix

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 var header = $('#header');
 
+var nav = header.find('nav');
+var totalHeaderHeight = header.height() - nav.height();
+
 window.addEventListener("scroll", function(event){
 	var position = $(document).scrollTop();
 
-	var nav = header.find('nav');
-	var totalHeaderHeight = header.height() - nav.height();
+	console.log(position);
+	console.log(totalHeaderHeight);
 
 	if(position > 1) {
-		if(!nav.hasClass('scroll')){
-			nav.removeClass('scroll');
-		}
-	} else {
 		nav.addClass('scroll');
+	} else {
+		nav.removeClass('scroll');
 	}
 
 	if(position > totalHeaderHeight) {
-		if(!nav.hasClass('shadow')){
-			nav.removeClass('shadow');
-		}
-	} else {
 		nav.addClass('shadow');
+	} else {
+		nav.removeClass('shadow');
 	}
 });
